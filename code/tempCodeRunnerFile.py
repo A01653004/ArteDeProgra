5e4df67c4bafe910a47b8fbed55098c0fbde64e6
BraunHdez <69830966+A01653004@users.noreply.github.com>
2021-10-27 10:58:37 -0500

    circle

--- code/tempCodeRunnerFile.py
+++ code/tempCodeRunnerFile.py
@@ -37,9 +37,14 @@ def square(start, end):
     end_fill()
 
 
-def circle(start, end):
+def circle_(start, end):
     "Draw circle from start to end."
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+    circle(end.x - start.x)
+    end_fill()
 
 
 def rectangle(start, end):
@@ -99,7 +104,7 @@ onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
-onkey(lambda: store('shape', circle), 'c')
+onkey(lambda: store('shape', circle_), 'c')
 onkey(lambda: store('shape', rectangle), 'r')
 onkey(lambda: store('shape', triangle), 't')
 done()
